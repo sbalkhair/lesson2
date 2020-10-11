20188cabc12abe8ef89271ff5a14394404785a5f
saeed <s.balkhair@hotmail.com>
2020-10-11 07:07:43 +0300

    Initial check-in of module lesson2

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace lesson2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // c# is sensitive language
+            //int number = 10;
+            //int Number = 20;
+
+            //Console.Write("Enter your age ");
+            //var input = Console.ReadLine();
+            //var age = Convert.ToInt32(input);
+            //Console.WriteLine($"Your age is {age}");
+
+            // + - * / %
+
+            //int number1 = 40;
+            //int number2 = 20;
+
+            ////10+30=30
+            //Console.WriteLine($"{number1}+{number2}={number1 + number2}");
+            //Console.WriteLine($"{number1}-{number2}={number1 - number2}");
+            //Console.WriteLine($"{number1}X{number2}={number1 * number2}");
+            //Console.WriteLine($"{number1}/{number2}={number1 / number2}");
+            //Console.WriteLine($"{number1}%{number2}={number1 % number2}");
+
+
+            //var number1 = 10;
+            //var number2 = 2;
+            // > < >= <= == !=
+
+
+            //Console.WriteLine($"{number1}>{number2}={number1 > number2}");
+            //Console.WriteLine($"{number1}<{number2}={number1 < number2}");
+            //Console.WriteLine($"{number1}>={number2}={number1 >= number2}");
+            //Console.WriteLine($"{number1}<={number2}={number1 <= number2}");
+            //Console.WriteLine($"{number1}=={number2}={number1 == number2}");
+            //Console.WriteLine($"{number1}!={number2}={number1 != number2}");
+
+            //var number1 = 10;
+            //var number2 = 2;
+            //var number3 = 5;
+            //var number4 = 2;
+
+            //Console.WriteLine($"{number1}>{number2} and {number3}<{number4} {number1 > number2 && number3 < number4}");
+            //Console.WriteLine($"{number1}>{number2} or {number3}<{number4} {number1 > number2 || number3 < number4}");
+
+
+            //var answer = true;
+            //Console.WriteLine($"Answer:{!answer}");
+
+            //int i = 1;
+            // i = i + 1;
+            //i += 1;
+            //i++;
+
+            #region MyRegion
+            Console.WriteLine("Region");
+            #endregion
+
+        }
+    }
+}
